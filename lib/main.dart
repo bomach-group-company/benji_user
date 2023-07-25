2c003160e2f921eb102f3c0c450056fc01b7ceb7
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-07-25 11:24:31 +0100

    added firebase auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:benji_user/theme/app%20theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
+import 'firebase_options.dart';
 // import 'app/home/home.dart';
 import 'routes/routes.dart';
-import 'splash screens/startup splash screen.dart';
+import 'src/splash screens/startup splash screen.dart';
 import 'theme/colors.dart';
 
 void main() async {
@@ -14,7 +17,9 @@ void main() async {
     ),
   );
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await SystemChrome.setPreferredOrientations(
     [DeviceOrientation.portraitUp],
   );
@@ -29,24 +34,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: "Benji",
       color: kPrimaryColor,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        scrollbarTheme: ScrollbarThemeData(
-          thumbColor: MaterialStateProperty.all(
-            kDarkGreyColor,
-          ),
-          radius: const Radius.circular(
-            6,
-          ),
-        ),
-        textTheme: GoogleFonts.senTextTheme(
-          // Update the textTheme property
-          Theme.of(context).textTheme, // Use the default theme
-        ),
-      ),
+      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       initialRoute: StartupSplashscreen.routeName,
       // home: Home(),
       routes: routes,
