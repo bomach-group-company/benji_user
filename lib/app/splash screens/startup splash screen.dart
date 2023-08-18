da4fcb20b200950a6399d930ae002ab66e9fe27f
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-08-18 10:41:31 +0100

    added a new package

---
+++ lib/app/splash screens/startup splash screen.dart
@@ -0,0 +1,71 @@
+// ignore_for_file: camel_case_types, file_names
+
+import 'package:benji_user/app/screens/onboarding%20screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../../theme/colors.dart';
+import '../../src/providers/constants.dart';
+
+class StartupSplashscreen extends StatefulWidget {
+  static String routeName = "Startup Splash Screen";
+  const StartupSplashscreen({super.key});
+
+  @override
+  State<StartupSplashscreen> createState() => _StartupSplashscreenState();
+}
+
+class _StartupSplashscreenState extends State<StartupSplashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (ctx) => const OnboardingScreen(),
+        ),
+      );
+    });
+    return Scaffold(
+      body: ListView(
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        physics: const BouncingScrollPhysics(),
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height / 4,
+                  width: MediaQuery.of(context).size.width / 2,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(
+                        "assets/images/splash screen/frame-1.png",
+                      ),
+                    ),
+                  ),
+                ),
+                kSizedBox,
+                const Center(
+                  child: Text(
+                    "User App",
+                    style: TextStyle(
+                      color: kTextBlackColor,
+                    ),
+                  ),
+                ),
+                kSizedBox,
+                SpinKitThreeInOut(
+                  color: kSecondaryColor,
+                  size: 20,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
