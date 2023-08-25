86bf35a9b66f5e066b7a0d65c8831cdd97abd58a
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-08-25 11:52:25 +0100

    removed future builder in address

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:benji_user/app/splash_screens/startup_splash_screen.dart';
 import 'package:benji_user/theme/app%20theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-import 'app/splash_screens/startup_splash_screen.dart';
 import 'theme/colors.dart';
 
 void main() {
