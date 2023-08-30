46c92fe1fab5aff1aafa40a7c95d98dea959d08e
Gideon Chukwuoma Chimemerie <egideonchuks@gmail.com>
2023-08-30 11:45:50 +0100

    finished working on the vendor location screen

--- lib/app/vendor/vendor_location.dart
+++ lib/app/vendor/vendor_location.dart
@@ -3,10 +3,8 @@
 import 'dart:async';
 import 'dart:ui' as ui; // Import the ui library with an alias
 
-import 'package:benji_user/src/providers/api_keys.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geolocator/geolocator.dart';
@@ -30,49 +28,30 @@ class _VendorLocationState extends State<VendorLocation> {
   @override
   void initState() {
     super.initState();
-    Timer(Duration(milliseconds: 500), () async {
-      setState(() {
-        _loadingScreen = true;
-      });
-      await Future.delayed(Duration(milliseconds: 1000));
-      setState(() {
-        _loadingScreen = false;
-      });
-    });
+    // _getPolyPoints();
+    _loadMapData();
   }
 
   //============================================================= ALL VARIABLES ======================================================================\\
-  Uint8List? _markerImage;
 
   //====================================== Setting Google Map Consts =========================================\\
 
-  static const LatLng _userLocation =
-      LatLng(6.455798292640031, 7.507804133159955);
+  Position? _userPosition;
+
   static const LatLng _vendorLocation =
       LatLng(6.463810164127019, 7.539888438605598);
   List<LatLng> _polylineCoordinates = [];
+  // List<LatLng> _latLng = <LatLng>[_userLocation, _vendorLocation];
+  Uint8List? _markerImage;
   List<Marker> _markers = <Marker>[];
-
-  List<MarkerId> _markerId = <MarkerId>[
-    MarkerId("0"),
-    MarkerId("1"),
-  ];
-
-  List<LatLng> _latLng = <LatLng>[_userLocation, _vendorLocation];
-
+  List<MarkerId> _markerId = <MarkerId>[MarkerId("0"), MarkerId("1")];
   List<String> _markerTitle = <String>["Me", "Ntachi Osa"];
-
   List<String> _markerSnippet = <String>["My Location", "4.7 Star Rating"];
-
   List<String> _customMarkers = <String>[
     "assets/icons/person_location.png",
     "assets/icons/store.png",
   ];
-
-//==========================================================================================\\
-
   //============================================================= BOOL VALUES ======================================================================\\
-  late bool _loadingScreen;
   bool _isExpanded = false;
 
   //========================================================== GlobalKeys ============================================================\\
@@ -81,24 +60,12 @@ class _VendorLocationState extends State<VendorLocation> {
   Completer<GoogleMapController> _googleMapController = Completer();
   GoogleMapController? _newGoogleMapController;
 
-  //============================================================== FUNCTIONS =============================================================================\\
-  void _handleRefresh() async {
-    setState(() {
-      _loadingScreen = true;
-    });
-    await _determinePosition();
-    setState(() {
-      _loadingScreen = false;
-    });
-  }
+  //============================================================== FUNCTIONS ===================================================================\\
 
   //======================================= Google Maps ================================================\\
 
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
+  /// When the location services are not enabled or permissions are denied the `Future` will return an error.
+  Future<void> _loadMapData() async {
     bool _serviceEnabled;
     LocationPermission _permission;
 
@@ -130,51 +97,59 @@ class _VendorLocationState extends State<VendorLocation> {
         'Location permissions are permanently denied, we cannot request permissions.',
       );
     }
+    await _getUserCurrentLocation();
+    await _loadCustomMarkers();
+    // await _getPolyPoints();
+  }
 
-    Position _position = await Geolocator.getCurrentPosition(
+//============================================== Get Current Location ==================================================\\
+
+  Future<Position> _getUserCurrentLocation() async {
+    Position _userLocation = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.high,
+    ).then(
+      (location) => _userPosition = location,
     );
 
-    LatLng _latLngPosition = LatLng(_position.latitude, _position.longitude);
+    LatLng _latLngPosition =
+        LatLng(_userLocation.latitude, _userLocation.longitude);
 
     CameraPosition _cameraPosition =
-        new CameraPosition(target: _latLngPosition, zoom: 13);
+        new CameraPosition(target: _latLngPosition, zoom: 12.68);
 
-    _newGoogleMapController!.animateCamera(
+    _newGoogleMapController?.animateCamera(
       CameraUpdate.newCameraPosition(_cameraPosition),
     );
 
-//============================================== Adding polypoints ==================================================\\
+    return _userLocation;
+  }
 
-    PolylinePoints _polyLinePoints = PolylinePoints();
-    PolylineResult _result = await _polyLinePoints.getRouteBetweenCoordinates(
-      googleMapsApiKey,
-      PointLatLng(_userLocation.latitude, _userLocation.longitude),
-      PointLatLng(_vendorLocation.latitude, _vendorLocation.longitude),
+  //====================================== Get bytes from assets =========================================\\
+
+  Future<Uint8List> _getBytesFromAssets(String path, int width) async {
+    ByteData _data = await rootBundle.load(path);
+    ui.Codec _codec = await ui.instantiateImageCodec(
+      _data.buffer.asUint8List(),
+      targetHeight: width,
     );
+    ui.FrameInfo _fi = await _codec.getNextFrame();
+    return (await _fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
 
-    if (_result.points.isNotEmpty) {
-      _result.points.forEach(
-        (PointLatLng point) =>
-            _polylineCoordinates.add(LatLng(point.latitude, point.longitude)),
-      );
-      setState(() {});
-    }
+  //====================================== Get Location Markers =========================================\\
 
-//====================================== Add Custom Markers =========================================\\
-
-    Future<Uint8List> _getBytesFromAssets(String path, int width) async {
-      ByteData _data = await rootBundle.load(path);
-      ui.Codec _codec = await ui.instantiateImageCodec(
-        _data.buffer.asUint8List(),
-        targetHeight: width,
-      );
-      ui.FrameInfo _fi = await _codec.getNextFrame();
-      return (await _fi.image.toByteData(format: ui.ImageByteFormat.png))!
-          .buffer
-          .asUint8List();
-    }
-
+  _loadCustomMarkers() async {
+    Position _userLocation = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    ).then(
+      (location) => _userPosition = location,
+    );
+    List<LatLng> _latLng = <LatLng>[
+      LatLng(_userLocation.latitude, _userLocation.longitude),
+      _vendorLocation
+    ];
     for (int i = 0; i < _customMarkers.length; i++) {
       final Uint8List _markerIcon =
           await _getBytesFromAssets(_customMarkers[i], 100);
@@ -192,30 +167,37 @@ class _VendorLocationState extends State<VendorLocation> {
       );
       setState(() {});
     }
-
-//==========================================================================================\\
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return _position;
   }
 
-//============================================== Get Current Location ==================================================\\
+  //============================================== Adding polypoints ==================================================\\
+  // _getPolyPoints() async {
+  //   Position _userLocation = await Geolocator.getCurrentPosition(
+  //     desiredAccuracy: LocationAccuracy.high,
+  //   ).then(
+  //     (location) => _userPosition = location,
+  //   );
+  //   PolylinePoints _polyLinePoints = PolylinePoints();
+  //   PolylineResult _result = await _polyLinePoints.getRouteBetweenCoordinates(
+  //     googleMapsApiKey,
+  //     PointLatLng(_userLocation.latitude, _userLocation.longitude),
+  //     PointLatLng(_vendorLocation.latitude, _vendorLocation.longitude),
+  //   );
 
-//============================================== Initial Camera Positon ============================================\\
+  //   if (_result.points.isNotEmpty) {
+  //     _result.points.forEach(
+  //       (PointLatLng point) =>
+  //           _polylineCoordinates.add(LatLng(point.latitude, point.longitude)),
+  //     );
+  //     setState(() {});
+  //   }
+  // }
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: _userLocation,
-    zoom: 13,
-  );
+//============================================== Create Google Maps ==================================================\\
 
   void _onMapCreated(GoogleMapController controller) {
     _googleMapController.complete(controller);
     _newGoogleMapController = controller;
-    _determinePosition();
   }
-
-//============================================================================================\\
 
 //========================================================== Navigation =============================================================\\
   void _viewProducts() => Get.off(
@@ -239,34 +221,24 @@ class _VendorLocationState extends State<VendorLocation> {
         title: "Vendors location",
         backgroundColor: kPrimaryColor,
         elevation: 0.0,
-        actions: [
-          IconButton(
-            onPressed: _handleRefresh,
-            icon: FaIcon(FontAwesomeIcons.arrowsRotate, color: kAccentColor),
-          ),
-        ],
+        actions: [],
         toolbarHeight: kToolbarHeight,
       ),
       body: Stack(
         children: [
-          _loadingScreen
-              ? SpinKitChasingDots(color: kAccentColor)
+          _userPosition == null
+              ? Center(child: SpinKitChasingDots(color: kAccentColor))
               : GoogleMap(
                   mapType: MapType.normal,
                   onMapCreated: _onMapCreated,
-                  initialCameraPosition: _kGooglePlex,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(
+                        _userPosition!.latitude, _userPosition!.longitude),
+                    zoom: 12.68,
+                  ),
                   markers: Set.of(_markers),
-                  // polylines: {
-                  //   Polyline(
-                  //     polylineId: PolylineId("route"),
-                  //     geodesic: true,
-                  //     color: kSecondaryColor,
-                  //     width: 6,
-                  //     points: _polylineCoordinates,
-                  //   ),
-                  // },
                   padding: EdgeInsets.only(
-                    bottom: _isExpanded ? mediaHeight * 0.56 : 90,
+                    bottom: _isExpanded ? mediaHeight * 0.32 : 90,
                   ),
                   compassEnabled: true,
                   mapToolbarEnabled: true,
