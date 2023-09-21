7d4f230b706f5d8f6a10d2241b1bec51fdde5b31
egideons <egideonchuks@gmail.com>
2023-09-21 10:10:10 +0100

    working on the payment sdk

--- lib/app/payment/monnify_payment_sdk.dart
+++ lib/app/payment/monnify_payment_sdk.dart
@@ -1,4 +1,7 @@
+import 'package:benji_user/src/common_widgets/button/my_elevatedbutton.dart';
+import 'package:benji_user/src/common_widgets/textformfield/number_textformfield.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../../src/providers/constants.dart';
 
@@ -10,6 +13,12 @@ class MonnifyPaymentSDK extends StatefulWidget {
 }
 
 class _MonnifyPaymentSDKState extends State<MonnifyPaymentSDK> {
+  final _priceEC = TextEditingController();
+  final _priceFN = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> _initializePayment() async {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +28,31 @@ class _MonnifyPaymentSDKState extends State<MonnifyPaymentSDK> {
           padding: const EdgeInsets.all(kDefaultPadding),
           children: [
             Form(
+              key: _formKey,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
-                children: [],
+                children: [
+                  NumberTextFormField(
+                    controller: _priceEC,
+                    validator: (value) {
+                      return null;
+                    },
+                    textInputAction: TextInputAction.go,
+                    focusNode: _priceFN,
+                    hintText: "Enter an amount",
+                    inputFormatter: [
+                      FilteringTextInputFormatter.digitsOnly,
+                    ],
+                  ),
+                  MyElevatedButton(
+                      title: "Pay",
+                      onPressed: (() async {
+                        if (_formKey.currentState!.validate()) {
+                          await _initializePayment();
+                        }
+                      }))
+                ],
               ),
             )
           ],
