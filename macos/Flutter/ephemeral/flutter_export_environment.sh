18dc8ed1f551688decba7c3ca418485c26976df0
egideons <egideonchuks@gmail.com>
2023-10-12 15:36:16 +0100

    added a chat interface using flutter_chat_ui

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Z-BOOK\benji"
+export "FLUTTER_ROOT=/Users/developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/developer/Documents/Work/App Development/benji_user"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
