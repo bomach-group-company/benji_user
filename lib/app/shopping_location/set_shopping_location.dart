97867ce02ae19c0e54195c65fb6cee234b622096
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-01-06 15:09:53 +0100

    added a scrollbar to the SetShoppingLocation widget

--- lib/app/shopping_location/set_shopping_location.dart
+++ lib/app/shopping_location/set_shopping_location.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
 import '../../src/components/button/my_elevatedbutton.dart';
-import '../../src/components/button/my_outlined_elevatedbutton.dart';
 import '../../src/providers/constants.dart';
 import '../../src/providers/responsive_constant.dart';
 import '../../theme/colors.dart';
@@ -23,8 +22,7 @@ class _SetShoppingLocationState extends State<SetShoppingLocation> {
   String cityValue = "";
   String address = "";
 
-  void setShoppingLocation() {}
-  void useCurrentLocation() {}
+  void setShoppingLocation(){}
 
   @override
   Widget build(BuildContext context) {
@@ -36,78 +34,66 @@ class _SetShoppingLocationState extends State<SetShoppingLocation> {
         actions: const [],
         backgroundColor: kPrimaryColor,
       ),
-      bottomNavigationBar: Container(
+      bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(kDefaultPadding),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            MyElevatedButton(
-              title: "Save",
-              onPressed: setShoppingLocation,
-            ),
-            kSizedBox,
-            MyOutlinedElevatedButton(
-              title: "Use my location",
-              onPressed: useCurrentLocation,
-            ),
-          ],
+        child: MyElevatedButton(
+          title: "Save",
+          onPressed: setShoppingLocation,
         ),
       ),
       body: SafeArea(
         maintainBottomViewPadding: true,
-        child: Scrollbar(
-          child: ListView(
-            physics: const BouncingScrollPhysics(),
-            scrollDirection: Axis.vertical,
-            padding: deviceType(media.width) > 2
-                ? const EdgeInsets.all(kDefaultPadding)
-                : const EdgeInsets.all(kDefaultPadding / 2),
-            children: [
-              Center(
-                child: Column(
-                  children: [
-                    Lottie.asset("assets/animations/shopping/frame_1.json"),
-                    kSizedBox,
-                    Text(
-                      "Vendors and Products are displayed based on your shopping location",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: kTextGreyColor,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w700,
-                      ),
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.vertical,
+          padding: deviceType(media.width) > 2
+              ? const EdgeInsets.all(kDefaultPadding)
+              : const EdgeInsets.all(kDefaultPadding / 2),
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  Lottie.asset("assets/animations/shopping/frame_1.json"),
+                  kSizedBox,
+                  Text(
+                    "Vendors and Products are displayed based on your shopping location",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: kTextGreyColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              kSizedBox,
-              CSCPicker(
-                layout: Layout.vertical,
-                defaultCountry: CscCountry.Nigeria,
-                currentCountry: "Nigeria",
-                currentState: "Enugu",
-                countryDropdownLabel: "Select a Country",
-                stateDropdownLabel: "Select a State",
-                cityDropdownLabel: "Select a City",
-                onCountryChanged: (value) {
-                  setState(() {
-                    countryValue = value;
-                  });
-                },
-                onStateChanged: (value) {
-                  setState(() {
-                    stateValue = value ?? "Enugu";
-                  });
-                },
-                onCityChanged: (value) {
-                  setState(() {
-                    cityValue = value ?? "";
-                  });
-                },
-              ),
-              kSizedBox,
-            ],
-          ),
+            ),
+            kSizedBox,
+            CSCPicker(
+              layout: Layout.vertical,
+              defaultCountry: CscCountry.Nigeria,
+              currentCountry: "Nigeria",
+              currentState: "Enugu",
+              countryDropdownLabel: "Select a Country",
+              stateDropdownLabel: "Select a State",
+              cityDropdownLabel: "Select a City",
+              onCountryChanged: (value) {
+                setState(() {
+                  countryValue = value;
+                });
+              },
+              onStateChanged: (value) {
+                setState(() {
+                  stateValue = value!;
+                });
+              },
+              onCityChanged: (value) {
+                setState(() {
+                  cityValue = value!;
+                });
+              },
+            ),
+            kSizedBox,
+          ],
         ),
       ),
     );
