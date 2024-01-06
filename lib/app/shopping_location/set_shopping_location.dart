fc95a06cd44be86cc1fcdd2f0c829b6b1a830b44
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-01-06 17:22:51 +0100

    optimising code and added location permission

--- lib/app/shopping_location/set_shopping_location.dart
+++ lib/app/shopping_location/set_shopping_location.dart
@@ -1,6 +1,9 @@
 import 'package:benji/src/components/appbar/my_appbar.dart';
+import 'package:benji/src/components/button/my_outlined_elevatedbutton.dart';
+import 'package:benji/src/repo/controller/error_controller.dart';
 import 'package:csc_picker/csc_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:lottie/lottie.dart';
 
 import '../../src/components/button/my_elevatedbutton.dart';
@@ -22,7 +25,65 @@ class _SetShoppingLocationState extends State<SetShoppingLocation> {
   String cityValue = "";
   String address = "";
 
-  void setShoppingLocation(){}
+  Future<void> checkLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.',
+      ).then((value) => ApiProcessorController.errorSnack(
+          "Location permissions are permanently denied, we cannot request permissions."));
+    }
+    if (permission == LocationPermission.whileInUse ||
+        permission == LocationPermission.always) {
+      ApiProcessorController.successSnack("Location is enabled already");
+    }
+  }
+
+  void setShoppingLocation() {
+    if (countryValue == "" || countryValue.isEmpty) {
+      ApiProcessorController.errorSnack("Please select a country");
+    }
+    if (stateValue == "" || stateValue.isEmpty) {
+      ApiProcessorController.errorSnack("Please select a state");
+    }
+    if (!stateValue.contains("Enugu")) {
+      ApiProcessorController.errorSnack(
+        "Sorry, we are only available in Enugu",
+      );
+    }
+    if (cityValue == "" || cityValue.isEmpty) {
+      ApiProcessorController.errorSnack("Please select a city");
+    }
+  }
+
+  void useCurrentLocation() async {
+    checkLocationPermission();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,9 +97,19 @@ class _SetShoppingLocationState extends State<SetShoppingLocation> {
       ),
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(kDefaultPadding),
-        child: MyElevatedButton(
-          title: "Save",
-          onPressed: setShoppingLocation,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            MyElevatedButton(
+              title: "Save",
+              onPressed: setShoppingLocation,
+            ),
+            kSizedBox,
+            MyOutlinedElevatedButton(
+              title: "Use my location",
+              onPressed: useCurrentLocation,
+            ),
+          ],
         ),
       ),
       body: SafeArea(
@@ -70,9 +141,7 @@ class _SetShoppingLocationState extends State<SetShoppingLocation> {
             kSizedBox,
             CSCPicker(
               layout: Layout.vertical,
-              defaultCountry: CscCountry.Nigeria,
-              currentCountry: "Nigeria",
-              currentState: "Enugu",
+              countryFilter: const [CscCountry.Nigeria],
               countryDropdownLabel: "Select a Country",
               stateDropdownLabel: "Select a State",
               cityDropdownLabel: "Select a City",
@@ -83,12 +152,12 @@ class _SetShoppingLocationState extends State<SetShoppingLocation> {
               },
               onStateChanged: (value) {
                 setState(() {
-                  stateValue = value!;
+                  stateValue = value ?? "";
                 });
               },
               onCityChanged: (value) {
                 setState(() {
-                  cityValue = value!;
+                  cityValue = value ?? "";
                 });
               },
             ),
