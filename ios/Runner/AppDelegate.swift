9af09b5dedd01d34ac5bff3364383fbedb7ebae4
egideons <egideonchuks@gmail.com>
2024-04-03 08:31:50 +0100

    continuing iteration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -20,8 +20,8 @@ import GoogleMaps
       SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in          
           SwiftAwesomeNotificationsPlugin.register(
             with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)          
-          FLTSharedPreferencesFoundationPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferencesfoundation.SharedPreferencesFoundationPlugin")!)
+          FLTSharedPreferencesPlugin.register(
+            with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
       }
 
       return super.application(application, didFinishLaunchingWithOptions: launchOptions)
