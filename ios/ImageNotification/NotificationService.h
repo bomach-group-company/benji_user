e95a545f83736501020d102d404aca76073ac1cb
egideons <egideonchuks@gmail.com>
2024-04-13 17:52:40 +0100

    continuing iteration

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Developer on 13/04/2024.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
