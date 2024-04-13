a5d8fecb80203b2f07cc07adca8c41e9cce40108
egideons <egideonchuks@gmail.com>
2024-04-13 12:41:19 +0100

    making final changes

--- lib/app/onboarding/onboarding_screens.dart
+++ lib/app/onboarding/onboarding_screens.dart
@@ -7,21 +7,22 @@ List<OnboardModel> screens = <OnboardModel>[
   OnboardModel(
     img: 'assets/images/onboarding_screen/frame-1.png',
     text: "Order from your favorite Stores",
-    subtext: "Have it delivered to any address \nof your choice",
+    subtext: "Have it delivered to any address \nof your choice.",
     bg: kSecondaryColor,
     button: kAccentColor,
   ),
   OnboardModel(
     img: 'assets/images/onboarding_screen/frame-2.png',
     text: "Quick and Safe \nDelivery",
-    subtext: "Get your packages safely and timely \ndelivered to your location",
+    subtext:
+        "Get your packages safely and timely \ndelivered to your location.",
     bg: kSecondaryColor,
     button: kAccentColor,
   ),
   OnboardModel(
     img: 'assets/images/onboarding_screen/frame-3.png',
     text: "Explore Varieties of \nProducts",
-    subtext: "Search for your preferred product \nbrand and specification",
+    subtext: "Search for your preferred product \nbrand and specification.",
     bg: kSecondaryColor,
     button: kAccentColor,
   ),
